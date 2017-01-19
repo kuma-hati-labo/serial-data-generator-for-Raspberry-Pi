55cb90995fe62641ebe2b1a0119b4ca5eb030787
ytaro <snark545.4@gmail.com>
2017-01-19 12:01:59 +0900

    ・コメント追加 ・不要な行の削除

--- serial-data-generator.py
+++ serial-data-generator.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
 
+#
+# シリアルポートから 0x00-0xFF の循環データを出力するプログラム
+#
+
 import sys
 import serial
 
@@ -16,7 +20,6 @@ ser = serial.Serial('/dev/ttyAMA0', int(argvs[1]))
 while 1:
 	for data in range(0, 256):
 		ser.write(chr(data))
-		print ".",
 
 ser.close()
 
